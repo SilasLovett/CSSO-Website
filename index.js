601008129b6e1cfc79ca114a3ca07c021b3eaf0e
SilasLovett <silaslovettinbox@gmail.com>
2024-07-29 18:53:47 -0500

    Page cleanup and animations
    Removed sections, will likely reintroduce them later
    Adjusted section size for consistency
    Changed animations
    Adjusted typography

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from("#descriptionSection", {
+/*gsap.from("#descriptionSection", {
     scrollTrigger: {
       trigger: "#descriptionSection",
       start: "top bottom", // top hits the bottom of the screen
@@ -9,4 +9,48 @@ gsap.from("#descriptionSection", {
     },
     autoAlpha: 0,
     x: '-100%',
+});*/
+
+gsap.from("#statsBox1", {
+  scrollTrigger: {
+    trigger: "#statsBox1",
+    start: "top bottom", // top hits the bottom of the screen
+    end: "top center+=100",
+    scrub: 0.25,
+  },
+  autoAlpha: 0,
+  x: '-100%',
+});
+
+gsap.from("#statsBox2", {
+  scrollTrigger: {
+    trigger: "#statsBox2",
+    start: "top bottom", // top hits the bottom of the screen
+    end: "top center+=100",
+    scrub: 0.25,
+  },
+  autoAlpha: 0,
+  x: '-100%',
+});
+
+gsap.from("#statsBox3", {
+  scrollTrigger: {
+    trigger: "#statsBox3",
+    start: "top bottom", // top hits the bottom of the screen
+    end: "top center+=100",
+    scrub: 0.25,
+  },
+  autoAlpha: 0,
+  x: '-100%',
+});
+
+gsap.from("#statsBox4", {
+  scrollTrigger: {
+    trigger: "#statsBox4",
+    start: "top bottom", // top hits the bottom of the screen
+    end: "top center+=100",
+    scrub: 0.25,
+  },
+  autoAlpha: 0,
+  x: '-100%',
 });
