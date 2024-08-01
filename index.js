ec8a7033f6fa855342f75a7a9c419f94db879ed4
SilasLovett <silaslovettinbox@gmail.com>
2024-08-01 18:13:14 -0500

    Added functionality to css editor
    Final commit of part 1!

--- index.js
+++ index.js
@@ -113,3 +113,69 @@ mobileNavBackButton.addEventListener('click', () => {
   mainLinks.style.left = "0";
 
 });
+
+
+// CSS EDITOR
+
+let CSSHeightInput = document.getElementById("CSSHeightInput");
+let CSSWidthInput = document.getElementById("CSSWidthInput");
+let CSSBRInput = document.getElementById("CSSBRInput");
+let CSSBCInput = document.getElementById("CSSBCInput");
+let CSSBackgroundInput = document.getElementById("CSSBackgroundInput");
+let CSSColorInput = document.getElementById("CSSColorInput");
+let CSSFontInput = document.getElementById("CSSFontInput");
+
+let CSSEditorButton = document.getElementById("CSSEditorButton");
+
+CSSHeightInput.addEventListener('input', () => {
+  CSSEditorButton.style.height = CSSHeightInput.value;
+});
+
+CSSWidthInput.addEventListener('input', () => {
+  CSSEditorButton.style.width = CSSWidthInput.value;
+});
+
+CSSBRInput.addEventListener('input', () => {
+  CSSEditorButton.style.borderRadius  = CSSBRInput.value;
+});
+
+CSSBCInput.addEventListener('input', () => {
+  CSSEditorButton.style.border = "2px solid " + CSSBCInput.value;
+});
+
+CSSBackgroundInput.addEventListener('input', () => {
+  CSSEditorButton.style.backgroundColor = CSSBackgroundInput.value;
+});
+
+CSSColorInput.addEventListener('input', () => {
+  CSSEditorButton.style.color = CSSColorInput.value;
+});
+
+CSSFontInput.addEventListener('input', () => {
+  CSSEditorButton.style.fontSize = CSSFontInput.value;
+});
+
+let CSSResetButton = document.getElementById("CSSResetButton");
+
+CSSResetButton.addEventListener('click', () => {
+  CSSEditorButton.style.height = "60px";
+  CSSHeightInput.value = "60px";
+
+  CSSEditorButton.style.width = "180px";
+  CSSWidthInput.value = "180px";
+
+  CSSEditorButton.style.borderRadius = "10px";
+  CSSBRInput.value = "10px";
+
+  CSSEditorButton.style.border = "2px solid white";
+  CSSBCInput.value = "white";
+
+  CSSEditorButton.style.backgroundColor = "black";
+  CSSBackgroundInput.value = "black";
+
+  CSSEditorButton.style.color = "white";
+  CSSColorInput.value = "white";
+
+  CSSEditorButton.style.fontSize = "1.25em";
+  CSSFontInput.value = "1.25em";
+});
